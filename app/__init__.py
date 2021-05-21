2506d92f677e07e4cfa4c5aad70c55e7cedab070
constantine <FerrumResistance@gmail.com>
2021-05-21 20:25:05 +0300

    commit from pycharm

--- app/__init__.py
+++ app/__init__.py
@@ -9,25 +9,32 @@ from flask_sqlalchemy import SQLAlchemy
 # after each change in 'models', need to run 'flask db migrate -m "COMMENT"'.
 # and then run 'flask db upgrade' - this will apply the migration script
 from flask_migrate import Migrate
-# login manager helper
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
 # standard flask method of  config.from_object(object)
 # we are assigning the class Config imported from above
 app.config.from_object(Config)
+
 # db is the database, it takes the SQLALCHEMY_DATABASE_URI from the CONFIG
 db = SQLAlchemy(app)
+
 # migrate helper for the db
 migrate = Migrate(app, db)
+
 # login uses the LoginManager class from flask-login
 login = LoginManager(app)
-# to handle permissions, Flask-Login needs to know what is the view function that handles logins
+
+# to handle permissions, Flask-Login needs to know what is the view function
+# that handles logins
 # The 'login' value is the function (or endpoint) name for the login view.
 login.login_view = 'login'
+
+# Initialize bootstrap
+bootstrap = Bootstrap(app)
 
 
 # then, we are importing the routes and models from the app
 from app import routes, models
-
-#this commit is test of Working Copy app
