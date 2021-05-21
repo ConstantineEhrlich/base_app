548329d7590dfade4dd5a6ee2a99eb1b711423eb
Constantine Ehrlich <ConstantineEhrlich@users.noreply.github.com>
2021-05-21 15:37:28 +0300

    commit from ipad

--- base_app.py
+++ base_app.py
@@ -5,6 +5,7 @@ from app.models import *
 
 # Then, it assigns some values to the environment so we can run Flask shell
 # and use the objects of 'db', 'User', 'Record', etc.
+# Test change in Textastic app and then push it from ipad
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Record': Record}
